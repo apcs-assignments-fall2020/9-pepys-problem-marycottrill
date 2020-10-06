df59bed93b564b78ff9d731c2df220b90369d6f5
marycottrill <21mcottrill@athenian.org>
2020-10-06 05:41:38 +0000

    Pepys Problem

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,26 +5,89 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int x = 0;
+        int timesrolled = 0;
+        int total = 0;
+
+        for (int i = 1; i <= 10000; i++){
+            x = 0;
+            for (int k = 1; k <= 6; k++){
+                int change = (int)(Math.random() * 6 + 1);
+                if (change == 6){
+                    x = x + 1;
+                } 
+            }
+            if (x > 0){
+                total++;
+            }
+            timesrolled++;
+
+        }
+        Double total2 = new Double(total);
+        Double times2 = new Double(timesrolled);
+        Double avg = (total2/times2) * 100;
+        System.out.println( "The average amount of six's rolled in six roles is " + avg);
+        return avg;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int x = 0;
+        int timesrolled= 0;
+        int total= 0;
+
+        for (int i = 1; i <= 1000000; i++){
+            x = 0;
+            for (int k = 1; k <= 12; k++){
+                int change = (int)(Math.random() * 6 + 1);
+                if (change == 6){
+                    x = x + 1;
+                } 
+            }
+            if (x > 1){
+                total++;
+            }
+            timesrolled++;
+        }
+        Double total2 = new Double(total);
+        Double times2 = new Double(timesrolled);
+        Double avg = (total2/times2) * 100;
+        System.out.println("The average amount of six's rolled in twelve roles is " + avg);
+        return avg;
     }
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int x = 0;
+        int timesrolled = 0;
+        int total = 0;
+        
+        for (int i = 1; i <= 10000000; i++){
+            x = 0;
+            for (int k = 1; k <= 18; k++){
+                int change = (int)(Math.random() * 6 + 1);
+                if (change == 6){
+                    x = x +1;
+                } 
+            }
+            if (x > 2){
+                total++;
+            }
+         
+            timesrolled++;
+        }
+        Double total2 = new Double(total);
+        Double times2 = new Double(timesrolled);
+        Double avg = (total2/times2) * 100;
+        System.out.println("The average amount of six's rolled in eighteen roles is " + avg);
+        return avg;
     }
-
-
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        probabilityOneSix();
+        probabilityTwoSixes();
+        probabilityThreeSixes();
+
     }
 }
